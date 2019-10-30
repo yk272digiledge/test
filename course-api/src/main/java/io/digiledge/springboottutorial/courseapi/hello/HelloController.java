39666c543be5bafa718927f615911db3cabf1f42
yk272 <yadukrishnan272@gmail.com>
2019-10-30 17:39:11 +0530

    first commit

---
+++ course-api/src/main/java/io/digiledge/springboottutorial/courseapi/hello/HelloController.java
@@ -0,0 +1,14 @@
+package io.digiledge.springboottutorial.courseapi.hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+}
